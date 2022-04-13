9a4e4639e9eb6bb9230ff5741835d79aa1af6755
ro <ro.michi1129@gmail.com>
2022-04-13 18:18:15 +0900

    Add chapter3

--- ch03/functions.py
+++ ch03/functions.py
@@ -19,7 +19,8 @@ def identity_function(x):
 
 
 def softmax(a):
-    exp_a = np.exp(a)
+    c = np.max(a)
+    exp_a = np.exp(a - c)
     sum_exp_a = np.sum(exp_a)
     y = exp_a / sum_exp_a
 
---
+++ dataset/README.md
@@ -0,0 +1,4 @@
+# keep
+
+「ゼロから作る DeepLearning」のコードをそのまま用いる
+https://github.com/oreilly-japan/deep-learning-from-scratch
